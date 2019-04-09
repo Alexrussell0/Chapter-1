761191b0348e78ce2ebc4d07593285e1429e4024
Alex Russell <alexrussellpersonal@gmail.com>
2019-04-09 12:35:18 -0500

    Initial commit

---
+++ src/TableAndChairs.java
@@ -0,0 +1,26 @@
+import static javax.swing.text.html.HTML.Tag.I;
+
+public class TableAndChairs {
+    //Author: Alex Russell
+    //Date: 04/09/2019
+    //Project: Tables and Chairs
+    public static void main(String[] args){
+
+        System.out.println("Building Some Tables And Chairs");
+        System.out.println("\n\n\n\n");
+
+        System.out.println("C                                                C");
+        System.out.println("H                                                H");
+        System.out.println("A                                                A");
+        System.out.println("I            XXXXXXXXXXXXXXXXXXXXXXXX            I");
+        System.out.println("R              X                  X              R");
+        System.out.println("CHAIRCHAIR     X                  X     CHAIRCHAIR");
+        System.out.println("C        C     X                  X     C        C");
+        System.out.println("H        H     X                  X     H        H");
+        System.out.println("A        A     X                  X     A        A");
+        System.out.println("I        I     X                  X     I        I");
+        System.out.println("R        R     X                  X     R        R");
+
+
+    }
+}
